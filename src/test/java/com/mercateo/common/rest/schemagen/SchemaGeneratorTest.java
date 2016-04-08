4d9e8f45281343ff2f49044f3c53af9c21e3e242
Dimitry Polivaev <dimitry.polivaev@mercateo.com>
2016-04-08 16:58:49 +0200

    Add factory classes for Links, PaginatedResonseBuilder, ListResponseBuilder, ObjectWithSchema and JsonHyperSchema

---
+++ src/test/java/com/mercateo/common/rest/schemagen/SchemaGeneratorTest.java
@@ -0,0 +1,61 @@
+package com.mercateo.common.rest.schemagen;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import javax.ws.rs.core.Link;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.mercateo.common.rest.schemagen.link.LinkFactory;
+import com.mercateo.common.rest.schemagen.link.SchemaGenerator;
+import com.mercateo.common.rest.schemagen.link.helper.MethodInvocation;
+import com.mercateo.common.rest.schemagen.link.relation.Rel;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SchemaGeneratorTest {
+    public static class Resource implements JerseyResource {
+        @SuppressWarnings("unused")
+        public void put(int id, int value) {
+            // for test
+        }
+    }
+
+    public static class SecondResource implements JerseyResource {
+        @SuppressWarnings("unused")
+        public int get(int id) {
+            return 0;
+        }
+    }
+
+    @Mock
+    private LinkFactory<Resource> linkFactoryForResource;
+
+    @Mock
+    private LinkFactory<SecondResource> linkFactoryForSecondResource;
+
+    @SuppressWarnings({ "unchecked" })
+    @Test
+    public void createsLinksForDifferentResources() throws Exception {
+        int id = 1;
+        int value = 2;
+        final MethodInvocation<Resource> putMethodInvocation = r -> r.put(id, value);
+        final MethodInvocation<SecondResource> getMethodInvocation = r -> r.get(id);
+        final Optional<Link> putLink = Optional.of(Link.fromPath("put").build());
+        final Optional<Link> getLink = Optional.of(Link.fromPath("get").build());
+        when(linkFactoryForResource.forCall(Rel.UPDATE, putMethodInvocation)).thenReturn(putLink);
+        when(linkFactoryForSecondResource.forCall(Rel.CANONICAL, getMethodInvocation)).thenReturn(
+                getLink);
+        assertThat(SchemaGenerator.<Resource> builder(linkFactoryForResource)//
+                .withLink(Rel.UPDATE, putMethodInvocation)
+                .withFactory(linkFactoryForSecondResource)
+                .withLink(Rel.CANONICAL, getMethodInvocation)
+                .build())//
+                        .containsExactly(putLink, getLink);
+    }
+}
