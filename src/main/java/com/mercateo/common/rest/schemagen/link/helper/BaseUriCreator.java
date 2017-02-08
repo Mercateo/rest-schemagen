db897f30ea9af4101526ade8f353089227edbc0c
Andreas Würl <andi@tryb.de>
2017-02-08 18:32:27 +0100

    header names should be handled case insensitive

--- src/main/java/com/mercateo/common/rest/schemagen/link/helper/BaseUriCreator.java
+++ src/main/java/com/mercateo/common/rest/schemagen/link/helper/BaseUriCreator.java
@@ -5,5 +5,19 @@ import java.util.List;
 import java.util.Map;
 
 public interface BaseUriCreator {
+    /**
+     * create base uri from request origin and raw request headers
+     * @param requestBaseUri default base Uri
+     * @param requestHeaders request headers as string multimap
+     * @return base uri for link targets
+     */
     URI createBaseUri(URI requestBaseUri, Map<String, List<String>> requestHeaders);
+
+    /**
+     * create base uri from request origin and wrapped request headers
+     * @param requestBaseUri default base Uri
+     * @param requestHeaders wrapped request headers
+     * @return base uri for link targets
+     */
+    URI createBaseUri(URI requestBaseUri, HttpRequestHeaders requestHeaders);
 }
