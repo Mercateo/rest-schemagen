2b9079a8f8c728ab45a62849592c76eee8aabb6b
joerg.adler <joerg.adler@mercateo.com>
2017-04-12 15:59:20 +0200

    only adding schema, if output is JSON

--- src/main/java/com/mercateo/common/rest/schemagen/link/LinkCreator.java
+++ src/main/java/com/mercateo/common/rest/schemagen/link/LinkCreator.java
@@ -1,22 +1,8 @@
 package com.mercateo.common.rest.schemagen.link;
 
-import com.google.common.collect.Iterables;
-import com.googlecode.gentyref.GenericTypeReflector;
-import com.mercateo.common.rest.schemagen.JsonSchemaGenerator;
-import com.mercateo.common.rest.schemagen.link.relation.Relation;
+import static com.mercateo.common.rest.schemagen.link.helper.ParameterAnnotationVisitor.visitAnnotations;
+import static java.util.Objects.requireNonNull;
 
-import javax.ws.rs.BeanParam;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.Link;
-import javax.ws.rs.core.Link.Builder;
-import javax.ws.rs.core.UriBuilder;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.lang.reflect.Type;
@@ -29,179 +15,205 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Stream;
 
-import static com.mercateo.common.rest.schemagen.link.helper.ParameterAnnotationVisitor.visitAnnotations;
-import static java.util.Objects.requireNonNull;
+import javax.ws.rs.BeanParam;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Link;
+import javax.ws.rs.core.Link.Builder;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriBuilder;
+
+import com.google.common.collect.Iterables;
+import com.googlecode.gentyref.GenericTypeReflector;
+import com.mercateo.common.rest.schemagen.JsonSchemaGenerator;
+import com.mercateo.common.rest.schemagen.link.relation.Relation;
 
 public class LinkCreator {
-    public static final String TARGET_SCHEMA_PARAM_KEY = "targetSchema";
+	public static final String TARGET_SCHEMA_PARAM_KEY = "targetSchema";
 
-    public static final String SCHEMA_PARAM_KEY = "schema";
+	public static final String SCHEMA_PARAM_KEY = "schema";
 
-    public static final String METHOD_PARAM_KEY = "method";
+	public static final String METHOD_PARAM_KEY = "method";
 
-    private final JsonSchemaGenerator jsonSchemaGenerator;
+	private final JsonSchemaGenerator jsonSchemaGenerator;
 
-    private final LinkFactoryContext linkFactoryContext;
+	private final LinkFactoryContext linkFactoryContext;
 
-    /**
-     * @param jsonSchemaGenerator
-     * @param linkFactoryContext
-     *
-     */
-    LinkCreator(JsonSchemaGenerator jsonSchemaGenerator, LinkFactoryContext linkFactoryContext) {
-        this.jsonSchemaGenerator = requireNonNull(jsonSchemaGenerator);
-        this.linkFactoryContext = linkFactoryContext;
-    }
+	/**
+	 * @param jsonSchemaGenerator
+	 * @param linkFactoryContext
+	 *
+	 */
+	LinkCreator(JsonSchemaGenerator jsonSchemaGenerator, LinkFactoryContext linkFactoryContext) {
+		this.jsonSchemaGenerator = requireNonNull(jsonSchemaGenerator);
+		this.linkFactoryContext = linkFactoryContext;
+	}
 
-    public static Builder setRelation(Relation relation, URI uri) {
-        requireNonNull(relation);
-        requireNonNull(uri);
-        Builder builder = Link.fromUri(uri).rel(relation.getName());
-        if (requireNonNull(relation).getType().isShouldBeSerialized()) {
-            builder.param("relType", relation.getType().getName());
-            builder.param("target", relation.getType().getSerializedName());
-        }
-        return builder;
-    }
+	public static Builder setRelation(Relation relation, URI uri) {
+		requireNonNull(relation);
+		requireNonNull(uri);
+		Builder builder = Link.fromUri(uri).rel(relation.getName());
+		if (requireNonNull(relation).getType().isShouldBeSerialized()) {
+			builder.param("relType", relation.getType().getName());
+			builder.param("target", relation.getType().getSerializedName());
+		}
+		return builder;
+	}
 
-    /**
-     * create a link for a resource method
-     *
-     * @param scopes   list of Scope objects for every scope level
-     * @param relation relation of method
-     * @return link with schema if applicable
-     */
-    public Link createFor(List<Scope> scopes, Relation relation) {
-        return createFor(scopes, relation, requireNonNull(linkFactoryContext));
-    }
+	/**
+	 * create a link for a resource method
+	 *
+	 * @param scopes
+	 *            list of Scope objects for every scope level
+	 * @param relation
+	 *            relation of method
+	 * @return link with schema if applicable
+	 */
+	public Link createFor(List<Scope> scopes, Relation relation) {
+		return createFor(scopes, relation, requireNonNull(linkFactoryContext));
+	}
 
-    /**
-     * create a link for a resource method
-     *
-     * @param scopes             list of Scope objects for every scope level
-     * @param relation           relation of method
-     * @param linkFactoryContext the base URI for resolution of relative URIs and method and property checkers
-     * @return link with schema if applicable
-     */
-    public Link createFor(List<Scope> scopes, Relation relation, LinkFactoryContext linkFactoryContext) {
-        final Class<?> resourceClass = scopes.get(0).getInvokedClass();
-        UriBuilder uriBuilder = UriBuilder.fromResource(resourceClass);
+	/**
+	 * create a link for a resource method
+	 *
+	 * @param scopes
+	 *            list of Scope objects for every scope level
+	 * @param relation
+	 *            relation of method
+	 * @param linkFactoryContext
+	 *            the base URI for resolution of relative URIs and method and
+	 *            property checkers
+	 * @return link with schema if applicable
+	 */
+	public Link createFor(List<Scope> scopes, Relation relation, LinkFactoryContext linkFactoryContext) {
+		final Class<?> resourceClass = scopes.get(0).getInvokedClass();
+		UriBuilder uriBuilder = UriBuilder.fromResource(resourceClass);
 
-        Map<String, Object> pathParameters = new HashMap<>();
-        for (Scope scope : scopes) {
-            final Method method = scope.getInvokedMethod();
-            final Object[] parameters = scope.getParams();
-            if (method.isAnnotationPresent(Path.class)) {
-                uriBuilder.path(method.getDeclaringClass(), method.getName());
-            }
-            pathParameters.putAll(collectPathParameters(scope, parameters));
-            setQueryParameters(uriBuilder, scope, parameters);
-        }
+		Map<String, Object> pathParameters = new HashMap<>();
+		for (Scope scope : scopes) {
+			final Method method = scope.getInvokedMethod();
+			final Object[] parameters = scope.getParams();
+			if (method.isAnnotationPresent(Path.class)) {
+				uriBuilder.path(method.getDeclaringClass(), method.getName());
+			}
+			pathParameters.putAll(collectPathParameters(scope, parameters));
+			setQueryParameters(uriBuilder, scope, parameters);
+		}
 
-        URI uri = mergeUri(linkFactoryContext.getBaseUri(), uriBuilder, pathParameters);
+		URI uri = mergeUri(linkFactoryContext.getBaseUri(), uriBuilder, pathParameters);
 
-        Builder builder = setRelation(relation, uri);
+		Builder builder = setRelation(relation, uri);
 
-        addLinkProperties(scopes, builder);
+		addLinkProperties(scopes, builder);
 
-        detectMediaType(scopes, builder);
+		detectMediaType(scopes, builder);
 
-        final Scope lastScopedMethod = Iterables.getLast(scopes);
-        addHttpMethod(builder, lastScopedMethod);
-        addSchemaIfNeeded(builder, lastScopedMethod, linkFactoryContext);
-        return builder.build();
-    }
+		final Scope lastScopedMethod = Iterables.getLast(scopes);
+		addHttpMethod(builder, lastScopedMethod);
+		addSchemaIfNeeded(builder, lastScopedMethod, linkFactoryContext);
+		return builder.build();
+	}
 
-    private URI mergeUri(URI baseUri, UriBuilder uriBuilder, Map<String, Object> pathParameters) {
-        URI uri = uriBuilder.buildFromMap(pathParameters);
+	private URI mergeUri(URI baseUri, UriBuilder uriBuilder, Map<String, Object> pathParameters) {
+		URI uri = uriBuilder.buildFromMap(pathParameters);
 
-        if (baseUri != null) {
-            UriBuilder mergedUriBuilder = UriBuilder.fromUri(baseUri);
-            mergedUriBuilder.path(uri.getPath());
-            mergedUriBuilder.replaceQuery(uri.getQuery());
-            return mergedUriBuilder.buildFromMap(pathParameters);
-        }
+		if (baseUri != null) {
+			UriBuilder mergedUriBuilder = UriBuilder.fromUri(baseUri);
+			mergedUriBuilder.path(uri.getPath());
+			mergedUriBuilder.replaceQuery(uri.getQuery());
+			return mergedUriBuilder.buildFromMap(pathParameters);
+		}
 
-        return uri;
-    }
+		return uri;
+	}
 
-    private void addLinkProperties(List<Scope> scopes, Builder builder) {
-        final LinkProperties properties = Iterables.getLast(scopes).getInvokedMethod()
-                .getAnnotation(LinkProperties.class);
-        if (properties != null) {
-            Stream.of(properties.value()).forEach(x -> builder.param(x.key(), x.value()));
-        }
-    }
+	private void addLinkProperties(List<Scope> scopes, Builder builder) {
+		final LinkProperties properties = Iterables.getLast(scopes).getInvokedMethod()
+				.getAnnotation(LinkProperties.class);
+		if (properties != null) {
+			Stream.of(properties.value()).forEach(x -> builder.param(x.key(), x.value()));
+		}
+	}
 
-    private void detectMediaType(Collection<Scope> scopes, Builder builder) {
-        final Method method = Iterables.getLast(scopes).getInvokedMethod();
-        Optional.ofNullable(method.getAnnotation(Produces.class)).ifPresent(produces -> {
-            final String[] values = produces.value();
-            if (values.length > 0) {
-                builder.param("mediaType", values[0]);
-            }
-        });
-    }
+	private void detectMediaType(Collection<Scope> scopes, Builder builder) {
+		detectMediaType(Iterables.getLast(scopes).getInvokedMethod())
+				.ifPresent(mediatype -> builder.param("mediaType", mediatype));
+	}
 
-    private Map<String, Object> collectPathParameters(Scope scope,
-                                                      Object[] parameters) {
-        final Map<String, Object> pathParameters = new HashMap<>();
-        visitAnnotations((parameter, parameterIndex, annotation) -> {
-            if (annotation instanceof PathParam) {
-                PathParam pathParamAnnotation = (PathParam) annotation;
-                pathParameters.put(pathParamAnnotation.value(), parameter);
-            } else if (annotation instanceof BeanParam) {
-                BeanParamExtractor beanParamExtractor = new BeanParamExtractor();
-                pathParameters.putAll(beanParamExtractor.getPathParameters(parameter));
-            }
-        }, scope.getInvokedMethod(), parameters);
+	private Optional<String> detectMediaType(Method method) {
+		return Optional.ofNullable(method.getAnnotation(Produces.class)).map(produces -> {
+			final String[] values = produces.value();
+			if (values.length > 0) {
+				return values[0];
+			}
+			return null;
+		});
 
-        return pathParameters;
-    }
+	}
 
-    private void setQueryParameters(final UriBuilder uriBuilder, Scope scope,
-                                    Object[] parameters) {
-        Type[] realParamTypes = GenericTypeReflector.getExactParameterTypes(scope
-                .getInvokedMethod(), scope.getInvokedClass());
-        visitAnnotations((parameter, parameterIndex, annotation) -> {
-            if (annotation instanceof QueryParam && parameter != null) {
-                uriBuilder.queryParam(((QueryParam) annotation).value(), parameter.toString());
-            } else if (annotation instanceof BeanParam && parameter != null) {
-                if (realParamTypes[parameterIndex] instanceof Class<?>) {
-                    BeanParamExtractor beanParamExtractor = new BeanParamExtractor();
-                    Map<String, Object[]> queryParameter = beanParamExtractor.getQueryParameters(
-                            parameter);
-                    queryParameter.forEach((uriBuilder::queryParam));
-                }
-            }
-        }, scope.getInvokedMethod(), parameters);
-    }
+	private Map<String, Object> collectPathParameters(Scope scope, Object[] parameters) {
+		final Map<String, Object> pathParameters = new HashMap<>();
+		visitAnnotations((parameter, parameterIndex, annotation) -> {
+			if (annotation instanceof PathParam) {
+				PathParam pathParamAnnotation = (PathParam) annotation;
+				pathParameters.put(pathParamAnnotation.value(), parameter);
+			} else if (annotation instanceof BeanParam) {
+				BeanParamExtractor beanParamExtractor = new BeanParamExtractor();
+				pathParameters.putAll(beanParamExtractor.getPathParameters(parameter));
+			}
+		}, scope.getInvokedMethod(), parameters);
 
-    private void addHttpMethod(Builder builder, Scope scope) {
-        final List<Class<? extends Annotation>> httpMethodAnnotations = Arrays.asList(GET.class,
-                POST.class, PUT.class, DELETE.class);
-        final Method invokedMethod = scope.getInvokedMethod();
-        final Optional<Class<? extends Annotation>> httpMethod = httpMethodAnnotations.stream()
-                .filter(invokedMethod::isAnnotationPresent).findFirst();
+		return pathParameters;
+	}
 
-        if (httpMethod.isPresent()) {
-            builder.param(METHOD_PARAM_KEY, httpMethod.get().getSimpleName());
-        } else {
-            throw new IllegalArgumentException(
-                    "LinkCreator: The method has to be annotated with one of: " + String.join(", ",
-                            (Iterable<String>) httpMethodAnnotations.stream().map(
-                                    Class::getSimpleName).map(m -> '@' + m)::iterator));
-        }
-    }
+	private void setQueryParameters(final UriBuilder uriBuilder, Scope scope, Object[] parameters) {
+		Type[] realParamTypes = GenericTypeReflector.getExactParameterTypes(scope.getInvokedMethod(),
+				scope.getInvokedClass());
+		visitAnnotations((parameter, parameterIndex, annotation) -> {
+			if (annotation instanceof QueryParam && parameter != null) {
+				uriBuilder.queryParam(((QueryParam) annotation).value(), parameter.toString());
+			} else if (annotation instanceof BeanParam && parameter != null) {
+				if (realParamTypes[parameterIndex] instanceof Class<?>) {
+					BeanParamExtractor beanParamExtractor = new BeanParamExtractor();
+					Map<String, Object[]> queryParameter = beanParamExtractor.getQueryParameters(parameter);
+					queryParameter.forEach((uriBuilder::queryParam));
+				}
+			}
+		}, scope.getInvokedMethod(), parameters);
+	}
 
-    private void addSchemaIfNeeded(Builder builder, Scope method, LinkFactoryContext linkFactoryContext) {
-        Optional<String> optionalInputSchema = jsonSchemaGenerator.createInputSchema(method,
-                linkFactoryContext.getFieldCheckerForSchema());
-        optionalInputSchema.ifPresent(s -> builder.param(SCHEMA_PARAM_KEY, s));
+	private void addHttpMethod(Builder builder, Scope scope) {
+		final List<Class<? extends Annotation>> httpMethodAnnotations = Arrays.asList(GET.class, POST.class, PUT.class,
+				DELETE.class);
+		final Method invokedMethod = scope.getInvokedMethod();
+		final Optional<Class<? extends Annotation>> httpMethod = httpMethodAnnotations.stream()
+				.filter(invokedMethod::isAnnotationPresent).findFirst();
 
-        Optional<String> optionalOutputSchema = jsonSchemaGenerator.createOutputSchema(method,
-                linkFactoryContext.getFieldCheckerForSchema());
-        optionalOutputSchema.ifPresent(s -> builder.param(TARGET_SCHEMA_PARAM_KEY, s));
-    }
+		if (httpMethod.isPresent()) {
+			builder.param(METHOD_PARAM_KEY, httpMethod.get().getSimpleName());
+		} else {
+			throw new IllegalArgumentException("LinkCreator: The method has to be annotated with one of: "
+					+ String.join(", ", (Iterable<String>) httpMethodAnnotations.stream().map(Class::getSimpleName)
+							.map(m -> '@' + m)::iterator));
+		}
+	}
+
+	private void addSchemaIfNeeded(Builder builder, Scope method, LinkFactoryContext linkFactoryContext) {
+		Optional<String> optionalInputSchema = jsonSchemaGenerator.createInputSchema(method,
+				linkFactoryContext.getFieldCheckerForSchema());
+		optionalInputSchema.ifPresent(s -> builder.param(SCHEMA_PARAM_KEY, s));
+		Optional<String> mt = detectMediaType(method.getInvokedMethod());
+		if (mt.isPresent() && MediaType.APPLICATION_JSON.equals(mt.get())) {
+			Optional<String> optionalOutputSchema = jsonSchemaGenerator.createOutputSchema(method,
+					linkFactoryContext.getFieldCheckerForSchema());
+			optionalOutputSchema.ifPresent(s -> builder.param(TARGET_SCHEMA_PARAM_KEY, s));
+		}
+	}
 
 }
--- src/test/java/com/mercateo/common/rest/schemagen/ResourceClass.java
+++ src/test/java/com/mercateo/common/rest/schemagen/ResourceClass.java
@@ -24,112 +24,113 @@ import com.mercateo.common.rest.schemagen.types.ObjectWithSchema;
 @SuppressWarnings("unused")
 public class ResourceClass implements JerseyResource {
 
-    @Inject
-    private LinkMetaFactory linkMetaFactory;
+	@Inject
+	private LinkMetaFactory linkMetaFactory;
 
-    @Inject
-    private ChildResourceClass childResource;
+	@Inject
+	private ChildResourceClass childResource;
 
-    @Path("/method/{id}")
-    @GET
-    @RolesAllowed("test")
-    public ObjectWithSchema<Something> getSomething(@PathParam("id") String id) {
-        Optional<Link> link = linkMetaFactory.createFactoryFor(ResourceClass.class).forCall(
-                Rel.SELF, r -> r.getSomething(id));
+	@Path("/method/{id}")
+	@GET
+	@RolesAllowed("test")
+	@Produces(MediaType.APPLICATION_JSON)
+	public ObjectWithSchema<Something> getSomething(@PathParam("id") String id) {
+		Optional<Link> link = linkMetaFactory.createFactoryFor(ResourceClass.class).forCall(Rel.SELF,
+				r -> r.getSomething(id));
 
-        return ObjectWithSchema.create(new Something(), JsonHyperSchema.from(link));
-    }
+		return ObjectWithSchema.create(new Something(), JsonHyperSchema.from(link));
+	}
 
-    @Path("/methods/{id}")
-    @GET
-    @RolesAllowed("test")
-    public ObjectWithSchema<Something[]> getSomethingArray(@PathParam("id") String id) {
-        Optional<Link> link = linkMetaFactory.createFactoryFor(ResourceClass.class).forCall(
-                Rel.SELF, r -> r.getSomething(id));
+	@Path("/methods/{id}")
+	@GET
+	@RolesAllowed("test")
+	public ObjectWithSchema<Something[]> getSomethingArray(@PathParam("id") String id) {
+		Optional<Link> link = linkMetaFactory.createFactoryFor(ResourceClass.class).forCall(Rel.SELF,
+				r -> r.getSomething(id));
 
-        return ObjectWithSchema.create(new Something[0], JsonHyperSchema.from(link));
-    }
+		return ObjectWithSchema.create(new Something[0], JsonHyperSchema.from(link));
+	}
 
-    @Path("/method")
-    @POST
-    @LinkProperties(@Entry(key="testKey", value="testValue"))
-    public void postSomething(Something something) {
-        // nothing
-    }
+	@Path("/method")
+	@POST
+	@LinkProperties(@Entry(key = "testKey", value = "testValue"))
+	public void postSomething(Something something) {
+		// nothing
+	}
 
-    @Path("/method/{id}")
-    @POST
-    public void postSomethingWithId(@PathParam("id") String id, @QueryParam("limit") int limit) {
-        // nothing
-    }
+	@Path("/method/{id}")
+	@POST
+	public void postSomethingWithId(@PathParam("id") String id, @QueryParam("limit") int limit) {
+		// nothing
+	}
 
-    @Path("/sub")
-    public ChildResourceClass getChildResource() {
-        return childResource;
-    }
+	@Path("/sub")
+	public ChildResourceClass getChildResource() {
+		return childResource;
+	}
 
-    @GET
-    @Path("{id2}")
-    public String getTwoIds(@PathParam("id") String id, @PathParam("id2") String id2) {
-        return "in two ids";
-    }
+	@GET
+	@Path("{id2}")
+	public String getTwoIds(@PathParam("id") String id, @PathParam("id2") String id2) {
+		return "in two ids";
+	}
 
-    public void noHttpMethod() {
-    }
+	public void noHttpMethod() {
+	}
 
-    @Path("parentResource")
-    public static class ParentResourceClass implements JerseyResource {
+	@Path("parentResource")
+	public static class ParentResourceClass implements JerseyResource {
 
-        @Inject
-        private ResourceClass resourceClass;
+		@Inject
+		private ResourceClass resourceClass;
 
-        @Path("subresource")
-        public ResourceClass getSubResource() {
-            return resourceClass;
-        }
+		@Path("subresource")
+		public ResourceClass getSubResource() {
+			return resourceClass;
+		}
 
-        @Path("{id}/subresource")
-        public ResourceClass getSubResourceForIdInParentResource() {
-            return resourceClass;
-        }
-    }
+		@Path("{id}/subresource")
+		public ResourceClass getSubResourceForIdInParentResource() {
+			return resourceClass;
+		}
+	}
 
-    public static class ChildResourceClass implements JerseyResource {
+	public static class ChildResourceClass implements JerseyResource {
 
-        @Inject
-        private LinkMetaFactory linkMetaFactory;
+		@Inject
+		private LinkMetaFactory linkMetaFactory;
 
-        @Path("/submethod/{id}")
-        @GET
-        @RolesAllowed("test")
-        public ObjectWithSchema<Something[]> getSomethingArray(@PathParam("id") String id) {
-            Optional<Link> link = linkMetaFactory.createFactoryFor(ResourceClass.class).subResource(
-                    ResourceClass::getChildResource, ChildResourceClass.class).forCall(Rel.SELF,
-                            r -> r.getSomething("12"));
+		@Path("/submethod/{id}")
+		@GET
+		@RolesAllowed("test")
+		public ObjectWithSchema<Something[]> getSomethingArray(@PathParam("id") String id) {
+			Optional<Link> link = linkMetaFactory.createFactoryFor(ResourceClass.class)
+					.subResource(ResourceClass::getChildResource, ChildResourceClass.class)
+					.forCall(Rel.SELF, r -> r.getSomething("12"));
 
-            return ObjectWithSchema.create(new Something[0], JsonHyperSchema.from(link));
-        }
+			return ObjectWithSchema.create(new Something[0], JsonHyperSchema.from(link));
+		}
 
-        @Path("/subsomething")
-        @GET
-        @RolesAllowed("test")
-        public ObjectWithSchema<Something> getSomething(@PathParam("id") String id) {
-            Optional<Link> link = linkMetaFactory.createFactoryFor(ResourceClass.class).forCall(
-                    Rel.SELF, r -> r.getSomething(id));
+		@Path("/subsomething")
+		@GET
+		@RolesAllowed("test")
+		public ObjectWithSchema<Something> getSomething(@PathParam("id") String id) {
+			Optional<Link> link = linkMetaFactory.createFactoryFor(ResourceClass.class).forCall(Rel.SELF,
+					r -> r.getSomething(id));
 
-            return ObjectWithSchema.create(new Something(), JsonHyperSchema.from(link));
-        }
+			return ObjectWithSchema.create(new Something(), JsonHyperSchema.from(link));
+		}
 
-        @Path("/submethod")
-        @POST
-        public void postSomething(Something something) {
-            // nothing
-        }
+		@Path("/submethod")
+		@POST
+		public void postSomething(Something something) {
+			// nothing
+		}
 
-        @Path("/submethod/{id}")
-        @POST
-        public void postSomethingWithId(@PathParam("id") String id, Something something) {
-            // nothing
-        }
-    }
+		@Path("/submethod/{id}")
+		@POST
+		public void postSomethingWithId(@PathParam("id") String id, Something something) {
+			// nothing
+		}
+	}
 }
